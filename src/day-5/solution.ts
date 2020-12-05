0c945ac6d9224f62a1d7ac827cae50c527cd80c8
Michael Lundin <mikel@ifit.com>
2020-12-05 13:30:09 -0800

    refactor: clean up pass processing

--- src/day-5/solution.ts
+++ src/day-5/solution.ts
@@ -3,34 +3,29 @@ import { processFile } from '../utils/file-reader';
 import { parsePass } from './boarding';
 
 export default class DayFiveSolution extends Solution {
-  private passArray: string[] = [];
+  private passIdArray: number[] = [];
 
   private async populateData(): Promise<void> {
-    if (this.passArray.length === 0) {
+    if (this.passIdArray.length === 0) {
       await processFile(this.file, line => {
-        this.passArray.push(line);
+        const parsedPass = parsePass(line);
+        const passId = parsedPass[0] * 8 + parsedPass[1];
+        this.passIdArray.push(passId);
       });
     }
   }
 
   public async executeFirstHalf(): Promise<number> {
     await this.populateData();
-    return this.passArray.reduce((max, currentPass) => {
-      const parsedPass = parsePass(currentPass);
-      return Math.max(max, parsedPass[0] * 8 + parsedPass[1]);
-    }, -1);
+    return Math.max(...this.passIdArray);
   }
 
   public async executeSecondHalf(): Promise<number> {
     await this.populateData();
-    const ids = this.passArray.map(pass => {
-      const parsedPass = parsePass(pass);
-      return parsedPass[0] * 8 + parsedPass[1];
-    });
-    ids.sort((a, b) => a - b);
+    this.passIdArray.sort((a, b) => a - b);
     let myId = -1;
-    ids.some((currentId, index) => {
-      if (currentId !== ids[index + 1] - 1) {
+    this.passIdArray.some((currentId, index) => {
+      if (currentId !== this.passIdArray[index + 1] - 1) {
         myId = currentId + 1;
         return true;
       }
